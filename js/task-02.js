ae39130db1709b26291822920093fceecf39bde0
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-04-09 00:23:16 +0300

    task2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+// console.log(...ingredientsItems)
+
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.append(...ingredientsItems);
