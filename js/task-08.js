26b2b5efc374a4054aa68660d49199b8f00c24ff
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-04-11 16:31:59 +0300

    task8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(loginForm);
+    const userData = {};
+
+    for (let [name, value] of formData) {
+        userData[name] = value;
+      }
+
+      if (!userData.email || !userData.password) {
+        alert('Заповніть всі поля');
+        return;
+      }
+    
+      console.log(userData);
+      loginForm.reset();
+});
