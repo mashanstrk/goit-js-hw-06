0abca536f671043b966891b52013e82488f1e61a
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-04-11 16:47:34 +0300

    task9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const currentColor = document.querySelector(".color");
+
+changeColorButton.addEventListener ('click', ()=> {
+const randomColor = getRandomHexColor();
+document.body.style.backgroundColor = randomColor;
+currentColor.textContent = randomColor;
+})
